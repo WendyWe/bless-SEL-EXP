be00bc3edba22b9897bb7627b4465fa64f4b1d4f
Wendy <kathy570602@gmail.com>
2025-10-27 20:04:27 +0800

    20251027 debug5

--- public/experimental/js/experimental.js
+++ public/experimental/js/experimental.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('/api/login', {
+            const response = await fetch('https://bless-sel-exp.onrender.com/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
