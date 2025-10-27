ec2441c51a223865e8645166a4fc0684aeb1455d
Wendy <kathy570602@gmail.com>
2025-10-27 21:30:57 +0800

    20251027 renew

--- public/experimental/daily_tasks/js/activityTracker.js
+++ public/experimental/daily_tasks/js/activityTracker.js
@@ -7,7 +7,7 @@ class ActivityTracker {
 
         // Tell server user started activity
         try {
-            const response = await fetch('/api/activity/start', {
+            const response = await fetch('https://bless-sel-exp.onrender.com/api/activity/start', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ 
@@ -34,7 +34,7 @@ class ActivityTracker {
 
         // Tell server user finished
         try {
-            await fetch('/api/activity/end', {
+            await fetch('https://bless-sel-exp.onrender.com/api/activity/end', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ 
